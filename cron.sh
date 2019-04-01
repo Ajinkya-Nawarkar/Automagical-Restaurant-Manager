e68e2c11d965383906fe6ba381f9d4d920b2011a
Ajinkya-Nawarkar <ajinkyanawarkar35@gmail.com>
2019-04-01 17:00:40 -0500

    updatd cron with correct URL

--- cron.sh
+++ cron.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd /home/an839/public_html/DCSP/DCSP_Project
+cd /home/an839/public_html/SE/Automagical-Restaurant-Manager
 
 git reset --hard HEAD
 LOCAL=git rev-parse HEAD
-REMOTE=git ls-remote https://github.com/Ajinkya-Nawarkar/DCSP_Project.git HEAD
+REMOTE=git ls-remote https://github.com/Ajinkya-Nawarkar/Automagical-Restaurant-Manager.git HEAD
 
 if ["$LOCAL" != "$REMOTE"]
 then
