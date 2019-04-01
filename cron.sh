b48eda57828573e1944ac21a8deaaa446a664159
Ajinkya-Nawarkar <ajinkyanawarkar35@gmail.com>
2019-04-01 16:13:13 -0500

    modified cron to trck order_dev

--- cron.sh
+++ cron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/an839/public_html/DCSP/DCSP_Project
+cd /home/an839/public_html/SE/link1/Automagical-Restaurant-Manager
 
 git reset --hard HEAD
 LOCAL=git rev-parse HEAD
@@ -8,9 +8,9 @@ REMOTE=git ls-remote https://github.com/Ajinkya-Nawarkar/DCSP_Project.git HEAD
 
 if ["$LOCAL" != "$REMOTE"]
 then
-    git checkout master
+    git checkout order_dev
     git checkout .
-    git pull origin master
+    git pull origin order_dev
 fi
 
 chmod -R 777 .
