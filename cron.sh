60202e88544ef1fb5ef4a49f1abd569d745eec88
Ajinkya-Nawarkar <ajinkyanawarkar35@gmail.com>
2019-04-04 14:18:04 -0500

    updated cron to trach host_frontend_dev

--- cron.sh
+++ cron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/an839/public_html/SE/Automagical-Restaurant-Manager
+cd /home/an839/public_html/SE/mike/Automagical-Restaurant-Manager
 
 git reset --hard HEAD
 LOCAL=git rev-parse HEAD
@@ -8,9 +8,9 @@ REMOTE=git ls-remote https://github.com/Ajinkya-Nawarkar/Automagical-Restaurant-
 
 if ["$LOCAL" != "$REMOTE"]
 then
-    git checkout master
+    git checkout host_frontend_dev
     git checkout .
-    git pull origin master
+    git pull origin host_frontend_Dev
 fi
 
 chmod -R 777 .
