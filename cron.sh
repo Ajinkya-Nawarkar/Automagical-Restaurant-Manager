ea512a560cacc5372d1f5f6621d4707a3ac37a3a
Ajinkya-Nawarkar <ajinkyanawarkar35@gmail.com>
2019-04-04 20:21:05 -0500

    updated cron to aid in merging with master

--- cron.sh
+++ cron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/an839/public_html/SE/link1/Automagical-Restaurant-Manager
+cd /home/an839/public_html/SE/Automagical-Restaurant-Manager
 
 git reset --hard HEAD
 LOCAL=git rev-parse HEAD
@@ -8,9 +8,9 @@ REMOTE=git ls-remote https://github.com/Ajinkya-Nawarkar/Automagical-Restaurant-
 
 if ["$LOCAL" != "$REMOTE"]
 then
-    git checkout order_dev
+    git checkout master
     git checkout .
-    git pull origin order_dev
+    git pull origin master
 fi
 
 chmod -R 777 .
