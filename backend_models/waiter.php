88c8806cb74661c3ebf2014888ca27550ae17ab6
Ajinkya-Nawarkar <ajinkyanawarkar35@gmail.com>
2019-04-05 20:16:04 -0500

    added initial structure and getter setter to Waiter class

--- backend_models/waiter.php
+++ backend_models/waiter.php
@@ -3,12 +3,33 @@
   	require_once(dirname(__DIR__)."/Database/dbAPI.php");
 	require_once(dirname(__DIR__)."/Backend_Models/employee.php");
 
-	class waiter
+	class Waiter extends Employee
 		{
+			private $isOccupied;
 	    	private $database;
-	    	function __construct()
+
+	    	function __construct($EID)
 	    	{
+	    		$this->EID = $EID;
+	    		$this->database = new dbAPI;
 	    	}
+
+	    	function getIsOccupied()
+	    	{
+	    		return $this->isOccupied;  
+	    	}
+
+	    	function setIsOccupied($occupied)
+	    	{
+	    		$this->isOccupied = $occupied; 
+	    	}
+
+	    	function getTableAssignment()
+	    	{
+	    		$this->database->get_waiter_table_assignment($this->EID);
+	    	}
+
+
 	    }
 
 ?>
