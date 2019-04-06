40749d09d8bf6726e2b71add116d8e8d2aa41cad
Ajinkya-Nawarkar <ajinkyanawarkar35@gmail.com>
2019-04-05 19:28:02 -0500

    Added assignTable() function to Host class

--- backend_models/host.php
+++ backend_models/host.php
@@ -3,11 +3,32 @@
   	require_once(dirname(__DIR__)."/Database/dbAPI.php");
 	require_once(dirname(__DIR__)."/Backend_Models/employee.php");
 
-	class host
+	class Host extends Employee
 		{
+	    	private $username;
 	    	private $database;
-	    	function __construct()
+
+	    	function __construct($username)
 	    	{
+	    		$this->username = $username;
+	    		$this->database = new dbAPI;
+	    	}
+
+	    	function assignTable()
+	    	{
+	    		$open_tables = $this->database->get_open_tables_list();
+	    		$open_table_TID = $open_tables[array_rand($open_tables)];
+	    		$table_obj = new Table($open_table_TID, TableState::open);
+
+	    		$free_waiters = $this->database->get_free_waiter_ID_list();
+	    		$free_waiter_EID = $free_waiters[array_rand($free_waiters)];
+	    		$waiter_obj = new Waiter($free_waiter_EID);
+
+	    		$this->database->set_waiter_table($free_waiter_EID, $open_table_TID);
+
+	    		$table_obj->setTableStateReserved();
+	    		$waiter_obj->setIsOccupied(true);
+
 	    	}
 	    }
 
