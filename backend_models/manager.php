f03d09e13ae5b9874b2d337141e8ae9c0ac14a47
Ajinkya-Nawarkar <ajinkyanawarkar35@gmail.com>
2019-04-06 23:05:53 -0500

    Added Manager class functions with some TODOs

--- backend_models/manager.php
+++ backend_models/manager.php
@@ -2,12 +2,38 @@
 
   	require_once(dirname(__DIR__)."/Database/dbAPI.php");
 
-	class manager
-		{
-	    	private $database;
-	    	function __construct()
-	    	{
-	    	}
-	    }
+	class Manager
+	{
+    	private $database;
+    	
+    	function __construct($EID)
+    	{
+    		$this->EID = $EID;
+    		$this->database = new dbAPI;
+    	}
+
+    	function getStatistics()
+    	{
+    		// TODO: Need to discuss in a team session
+    	}
+
+    	function addEmployee($username, EmployeePositions $position)
+    	{
+    		$newEID = $this->database->get_employee_table_size();
+    		$this->database->addEmployee($newEID, $username, $position);
+    	}
+
+    	function removeEmployee($EID)
+    	{
+    		$this->database->remove_employee($EID);
+    	}
+
+    	function getEmployees()
+    	{
+    		// depends on how database call is implemented
+    		return $this->database->get_all_employees();
+    	}
+
+    }
 
 ?>
