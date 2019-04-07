1a023b3a11f56bb8101e6dec7198f3b97bb9bbe2
gauravj09 <gsj38@msstate.edu>
2019-04-06 23:42:20 -0500

    Updated PHP file to fix formatting issues

--- backend_models/busser.php
+++ backend_models/busser.php
@@ -1,39 +1,40 @@
 <?php
 
-  require_once(dirname(__DIR__)."/Database/dbAPI.php");
-  require_once(dirname(__DIR__)."/Backend_Models/employee.php");
+	require_once(dirname(__DIR__)."/Database/dbAPI.php");
+	require_once(dirname(__DIR__)."/Backend_Models/employee.php");
 
-  class Busser extends Employee
+	class Busser extends Employee
 	{
-	private $isOccupied;
-	private $table_obj;
-	private $database;
+		private $isOccupied;
+		private $table_obj;
+		private $database;
 
-	function __construct($EID)
-	{
-	  $this->EID = $EID;
-	  $this->isOccupied = false;
-	  $this->database = new dbAPI;
-	}
-
-	function getDirtyTables()
-	{
-	  $unclean_tables = $this->database->get_unclean_tables_list();
-
-	  if($unclean_tables)
+		function __construct($EID)
 		{
-		  $unclean_table_TID = $unclean_tables[array_rand($unclean_tables)];
-		  $table_obj = new Table($unclean_tables_TID);
-		  return $unclean_table_TID;
+			$this->EID = $EID;
+			$this->isOccupied = false;
+			$this->database = new dbAPI;
 		}
+
+		function getDirtyTables()
+		{
+			$unclean_tables = $this->database->get_unclean_tables_list();
+
+			if($unclean_tables)
+			{
+				$unclean_table_TID = $unclean_tables[array_rand($unclean_tables)];
+				$table_obj = new Table($unclean_tables_TID);
+				return $unclean_table_TID;
+			}
+
 		return -1;
-	}
+		}
 
-	function setTableStateOpen()
-	{
-		$this->table_obj->setTableStateOpen();
-	}
+		function setTableStateOpen()
+		{
+			$this->table_obj->setTableStateOpen();
+		}
 
-  }
+	}
 
 ?>
