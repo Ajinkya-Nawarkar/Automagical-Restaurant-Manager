ce4853e656697be871a7a8bd859e3ee1b5e6bea9
gauravj09 <gsj38@msstate.edu>
2019-04-07 18:14:34 -0500

    Updated getOrder() function to fix receive orders

--- backend_models/cook.php
+++ backend_models/cook.php
@@ -1,30 +1,29 @@
 <?php
 
-  	require_once(dirname(__DIR__)."/Database/dbAPI.php");
+	require_once(dirname(__DIR__)."/Database/dbAPI.php");
 	require_once(dirname(__DIR__)."/Backend_Models/employee.php");
 
 	class Cook extends Employee
 		{
 			private $order;
-	    	private $database;
+			private $database;
 
-	    	function __construct($EID)
+			function __construct($EID)
 			{	
 				$this->EID = $EID;
-				$this->order = 1; //needs to be updated
 				$this->database = new dbAPI;
-	    	}	
+			}	
 
 			function getOrder()
 			{
-				return $this->order;
+				return $this->database->get_order();
 			}
 			
-	    	function setOrderReady($OID)
-	    	{
-	    		$this->order = new Order($OID);
-	    		$this->order->setOrderReady();
+			function setOrderReady($OID)
+			{
+				$this->order = new Order($OID);
+				$this->order->setOrderReady();
 			}
-	    }
+		}
 
 ?>
