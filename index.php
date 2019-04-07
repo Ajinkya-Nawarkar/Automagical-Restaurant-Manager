f04622344ac88e538e2c4e0909c03defa32d38e6
chayapatt <chayapatt@hotmail.com>
2019-04-07 16:45:14 -0500

    front page simple form

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
+<!DOCTYPE html>
+
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>It\'s me. Testing</p>'; ?>
- </body>
+<head>
+<title>FrontPage</title>
+</head>
+
+<h1>Automagical Restaurant Manager</h1>
+<br>
+<h3>Login</h3>
+
+<form action = "/">
+  Username: <br>
+  <input type = "text" name = "username">
+  <br>
+  Password: <br>
+  <input type = "text" name = "password">
+  <br><br>
+  <input type = "submit" value = "Login">
+</form>
 </html>
