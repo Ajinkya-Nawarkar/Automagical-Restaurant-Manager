4893a4e0fda852fa65501e5def1d315892ce7608
gauravj09 <gsj38@msstate.edu>
2019-04-07 18:02:54 -0500

    Created getOrder() and setOrderReady() functions for Cook Class

--- backend_models/cook.php
+++ backend_models/cook.php
@@ -3,12 +3,28 @@
   	require_once(dirname(__DIR__)."/Database/dbAPI.php");
 	require_once(dirname(__DIR__)."/Backend_Models/employee.php");
 
-	class cook
+	class Cook extends Employee
 		{
+			private $order;
 	    	private $database;
-	    	function __construct()
+
+	    	function __construct($EID)
+			{	
+				$this->EID = $EID;
+				$this->order = 1; //needs to be updated
+				$this->database = new dbAPI;
+	    	}	
+
+			function getOrder()
+			{
+				return $this->order;
+			}
+			
+	    	function setOrderReady($OID)
 	    	{
-	    	}
+	    		$this->order = new Order($OID);
+	    		$this->order->setOrderReady();
+			}
 	    }
 
 ?>
