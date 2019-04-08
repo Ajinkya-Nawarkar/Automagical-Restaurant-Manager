7e600c4b3e259642a0352cd3ea4ed226045da13f
Gaurav Joshi <gsj38@msstate.edu>
2019-04-08 00:05:27 -0500

    Bcook dev (#9)
    * Created getOrder() and setOrderReady() functions for Cook Class
    
    * Updated getOrder() function to fix receive orders
    
    * Fixed incorrect function call in setIsReady() and updated the getOrder() function to getRecentOrder() to be more descriptive

--- backend_models/cook.php
+++ backend_models/cook.php
@@ -1,14 +1,30 @@
 <?php
 
-  	require_once(dirname(__DIR__)."/Database/dbAPI.php");
+	require_once(dirname(__DIR__)."/Database/dbAPI.php");
 	require_once(dirname(__DIR__)."/Backend_Models/employee.php");
+	require_once(dirname(__DIR__)."/Backend_Models/order.php");
 
-	class cook
+	class Cook extends Employee
 		{
-	    	private $database;
-	    	function __construct()
-	    	{
-	    	}
-	    }
+			private $order;
+			private $database;
+
+			function __construct($EID)
+			{	
+				$this->EID = $EID;
+				$this->database = new dbAPI;
+			}	
+
+			function getRecentOrder()
+			{
+				return $this->database->get_recent_order();
+			}
+			
+			function setIsReady($OID)
+			{
+				$this->order = new Order($OID);
+				$this->order->setIsReady();
+			}
+		}
 
 ?>
