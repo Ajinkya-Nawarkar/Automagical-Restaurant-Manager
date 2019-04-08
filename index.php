4bbe65d70585d89b07f27b3ea647910c86ee1d3d
michael-poole <mpoole4@comcast.net>
2019-04-08 11:35:02 -0500

    Resolved conflict

--- index.php
+++ index.php
@@ -3,10 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
-<<<<<<< HEAD
  <?php echo '<p>Hello michaels World</p>'; ?>
-=======
- <?php echo '<p>Hello f****** World</p>'; ?>
->>>>>>> master
  </body>
 </html>
