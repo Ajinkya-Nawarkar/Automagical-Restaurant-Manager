7738d7dc36428e5a3825f73dde12ec8e1a4455e3
Ajinkya Nawarkar <ajinkyanawarkar35@gmail.com>
2019-04-08 21:59:12 -0500

    Added Manager class functions with some TODOs (#7)
    * Added Manager class functions with some TODOs
    
    * added the missing extension to employee class

--- backend_models/manager.php
+++ backend_models/manager.php
@@ -1,13 +1,40 @@
 <?php
 
   	require_once(dirname(__DIR__)."/Database/dbAPI.php");
+    require_once(dirname(__DIR__)."/Backend_Models/employee.php");
 
-	class manager
-		{
-	    	private $database;
-	    	function __construct()
-	    	{
-	    	}
-	    }
+	class Manager extends Employee
+	{
+    	private $database;
+    	
+    	function __construct($EID)
+    	{
+    		$this->EID = $EID;
+    		$this->database = new dbAPI;
+    	}
+
+    	function getStatistics()
+    	{
+    		// TODO: Need to discuss in a team session
+    	}
+
+    	function addEmployee($username, EmployeePositions $position)
+    	{
+    		$newEID = $this->database->get_employee_table_size();
+    		$this->database->addEmployee($newEID, $username, $position);
+    	}
+
+    	function removeEmployee($EID)
+    	{
+    		$this->database->remove_employee($EID);
+    	}
+
+    	function getEmployees()
+    	{
+    		// depends on how database call is implemented
+    		return $this->database->get_all_employees();
+    	}
+
+    }
 
 ?>
