a51c68b0f9347b45adebb0a4cf8e1deb2d612546
Ajinkya Nawarkar <ajinkyanawarkar35@gmail.com>
2019-04-19 15:45:25 -0500

    removed conflict markers

--- database/dbAPI.php
+++ database/dbAPI.php
@@ -13,13 +13,8 @@
         die($this->connection->connect_error);
       }
     }
-<<<<<<< HEAD
     
     // return employee (for login.php)
-=======
-
-    // return employee
->>>>>>> a697b51a80d5c60e9d8b0e08c44886e014cdcdee
     public function getEmployee($username) {
       $query = "SELECT * FROM ARM_Employee WHERE username='". $username . "'";
       $result = $this->connection->query($query);
