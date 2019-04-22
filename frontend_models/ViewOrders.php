86920f71f0e331e076ff6945107837670ae6b0f5
chayapatt <chayapatt@hotmail.com>
2019-04-21 22:18:38 -0500

    cook page to master

--- frontend_models/ViewOrders.php
+++ frontend_models/ViewOrders.php
@@ -9,49 +9,35 @@
 
 <body>
 
+<h1>Cook</h1>
+<button onclick = "setOrderReady()">Order Ready</button>
+
 <?php
-require_once('cook.php');
-$things1 = new cook;
-require_once('order.php');
-$things2 = new order;
+require_once('backend_models/cook.php');
 
 $order = $things1->getRecentOrder();
 $OID = $order['OID'];
 
-while (sizeof($order) > 0)
+
+while ($order != -1)
 {
-    $itemListDeserialized = deserialize($order['order']);
-    if ($things2->getIsReady($OID) == false)
-    echo "$itemListDeserialized";
+    $itemListDeserialized = deserialize($order['itemList']);
+    $showItem = 0;
+    if ($showItem == 0)
+    {
+    echo "Item for order " . $OID . ": ";
+    echo "<br>";
+    echo $itemListDeserialized;
+    $showItem = 1;
+    }
+}
+
+function setOrderReady($OID)
+{
+    $things1->setIsReady($OID);
+    $showItem = 0;
 }
 ?>
 
-<h1>Cook</h1>
-<button onclick = "showOrder()">Show Order</button>
-<button onclick = "$things1->setIsReady($OID)">Order Ready</button>
-
-<div id = "order" style = "display:none">
-    <h2>Order:</h2>
-    <p>Item 1</p>
-    <p>Item 2</p>
-    <p>Item 3</p>
-</div>
-
-
-<script>
-    function showOrder()
-    {
-        var x = document.getElementById("order");
-          if (x.style.display === "none")
-          x.style.display = "block";
-    }
-    
-    /*function orderReady()
-    {
-        var x = document.getElementById("order");
-          if (x.style.display === "block")
-          x.style.display = "none";
-    }*/
-</script>
 </body>
 </html>
